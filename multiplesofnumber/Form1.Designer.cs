2e2da75be3fb0aa15d82d010b828c16957b7913e
Ramazan Sarı <rmznsrii7@gmail.com>
2024-05-03 14:59:14 +0300

    Add project files.

---
+++ multiplesofnumber/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace multiplesofnumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rtxtValue = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdBlack = new System.Windows.Forms.RadioButton();
+            this.rdBlue = new System.Windows.Forms.RadioButton();
+            this.rdRed = new System.Windows.Forms.RadioButton();
+            this.rdGreen = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbKat = new System.Windows.Forms.ComboBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtxtValue
+            // 
+            this.rtxtValue.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtxtValue.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtxtValue.Location = new System.Drawing.Point(0, 177);
+            this.rtxtValue.Name = "rtxtValue";
+            this.rtxtValue.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.rtxtValue.Size = new System.Drawing.Size(420, 143);
+            this.rtxtValue.TabIndex = 17;
+            this.rtxtValue.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdBlack);
+            this.groupBox1.Controls.Add(this.rdBlue);
+            this.groupBox1.Controls.Add(this.rdRed);
+            this.groupBox1.Controls.Add(this.rdGreen);
+            this.groupBox1.Location = new System.Drawing.Point(316, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(83, 120);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Renk Seçimi";
+            // 
+            // rdBlack
+            // 
+            this.rdBlack.AutoSize = true;
+            this.rdBlack.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdBlack.ForeColor = System.Drawing.Color.Black;
+            this.rdBlack.Location = new System.Drawing.Point(7, 93);
+            this.rdBlack.Name = "rdBlack";
+            this.rdBlack.Size = new System.Drawing.Size(56, 17);
+            this.rdBlack.TabIndex = 3;
+            this.rdBlack.TabStop = true;
+            this.rdBlack.Text = "Siyah";
+            this.rdBlack.UseVisualStyleBackColor = true;
+            this.rdBlack.CheckedChanged += new System.EventHandler(this.rdBlack_CheckedChanged);
+            // 
+            // rdBlue
+            // 
+            this.rdBlue.AutoSize = true;
+            this.rdBlue.ForeColor = System.Drawing.Color.Blue;
+            this.rdBlue.Location = new System.Drawing.Point(7, 70);
+            this.rdBlue.Name = "rdBlue";
+            this.rdBlue.Size = new System.Drawing.Size(48, 17);
+            this.rdBlue.TabIndex = 2;
+            this.rdBlue.TabStop = true;
+            this.rdBlue.Text = "Mavi";
+            this.rdBlue.UseVisualStyleBackColor = true;
+            this.rdBlue.CheckedChanged += new System.EventHandler(this.rdBlue_CheckedChanged);
+            // 
+            // rdRed
+            // 
+            this.rdRed.AutoSize = true;
+            this.rdRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdRed.ForeColor = System.Drawing.Color.Red;
+            this.rdRed.Location = new System.Drawing.Point(7, 47);
+            this.rdRed.Name = "rdRed";
+            this.rdRed.Size = new System.Drawing.Size(61, 17);
+            this.rdRed.TabIndex = 1;
+            this.rdRed.TabStop = true;
+            this.rdRed.Text = "Kırmızı";
+            this.rdRed.UseVisualStyleBackColor = true;
+            this.rdRed.CheckedChanged += new System.EventHandler(this.rdRed_CheckedChanged);
+            // 
+            // rdGreen
+            // 
+            this.rdGreen.AutoSize = true;
+            this.rdGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdGreen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.rdGreen.Location = new System.Drawing.Point(7, 24);
+            this.rdGreen.Name = "rdGreen";
+            this.rdGreen.Size = new System.Drawing.Size(52, 17);
+            this.rdGreen.TabIndex = 0;
+            this.rdGreen.TabStop = true;
+            this.rdGreen.Text = "Yeşil";
+            this.rdGreen.UseVisualStyleBackColor = true;
+            this.rdGreen.CheckedChanged += new System.EventHandler(this.rdGreen_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(25, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Katı seçiniz";
+            // 
+            // cmbKat
+            // 
+            this.cmbKat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKat.FormattingEnabled = true;
+            this.cmbKat.Location = new System.Drawing.Point(29, 130);
+            this.cmbKat.Name = "cmbKat";
+            this.cmbKat.Size = new System.Drawing.Size(121, 21);
+            this.cmbKat.TabIndex = 14;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi2.Location = new System.Drawing.Point(170, 41);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(121, 24);
+            this.txtSayi2.TabIndex = 13;
+            this.txtSayi2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayi2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(166, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "İkinci sayıyı giriniz";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi1.Location = new System.Drawing.Point(29, 41);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(121, 24);
+            this.txtSayi1.TabIndex = 11;
+            this.txtSayi1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayi1_KeyPress);
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(170, 107);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(121, 54);
+            this.btnGoster.TabIndex = 10;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "İlk sayıyı giriniz";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 320);
+            this.Controls.Add(this.rtxtValue);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbKat);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Katlı Sıralar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtValue;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdBlack;
+        private System.Windows.Forms.RadioButton rdBlue;
+        private System.Windows.Forms.RadioButton rdRed;
+        private System.Windows.Forms.RadioButton rdGreen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbKat;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ multiplesofnumber/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiplesofnumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Verilerin gösterileceği richTextBox'ı yazmaya kapatıyoruz.
+            rtxtValue.ReadOnly = true;
+            cmbKat.Items.Add("2"); //ComboBox'a item ekliyoruz: integer türünde, kat olacak sayılar giriyoruz. 
+            cmbKat.Items.Add("3");
+            cmbKat.Items.Add("4");
+            cmbKat.Items.Add("5");
+            cmbKat.Items.Add("6");
+            cmbKat.Items.Add("7");
+            cmbKat.Items.Add("8");
+            cmbKat.Items.Add("9");
+            cmbKat.Items.Add("10");
+        }
+
+        private void txtSayi1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar); //sadece sayı girilsin.
+        }
+
+        private void txtSayi2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar); //sadece sayı girilsin.
+        }
+        int sayi1, sayi2, kat;
+
+        private void rdBlack_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdBlack.Checked) rtxtValue.ForeColor = Color.Black; //Siyah
+        }
+
+        private void rdGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdGreen.Checked) rtxtValue.ForeColor = Color.Green; //Yeşil
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            if (txtSayi1.Text.Trim() == "") //textbox içinin boş olup olmadığını kontrol eder. Eğer hiçbir karakter yoksa veya yalnızca boşluk varsa... 
+            {
+                errorProvider1.SetError(txtSayi1, "Alanı Doldurun!"); //error provider'ın ilgili textbox da kullanımı.
+                MessageBox.Show("Lütfen sayıları giriniz!", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (txtSayi2.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtSayi2, "Alanı Doldurun!");
+                MessageBox.Show("Lütfen sayıları giriniz!", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (cmbKat.SelectedIndex == -1) // comboboxlarda seçim yapılmadıysa index değerleri -1 olur. Seöim yapılmadığında uyarı vermesi için bu kodları yazıyoruz.
+            {
+                errorProvider1.SetError(cmbKat, "Kat Sayısı Seçin!");
+                MessageBox.Show("Lütfen sayılar arasında hesaplanacak kat sayısını seçiniz!", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else //Yukarıdaki if'ler textboxların ve comboboxların dolu olup olmadığını kontrol eden bloklardı. Eğer hepsi doluysa hiç bir if çalışmaz ve else durumu çalışır.
+            {
+                rtxtValue.Clear(); // Her butona basılışta richtextbox'da gösterilen verileri 0'la yıp aşağıdaki kod bloklarını çalıştırması için yazdım. Bilgiler üst üste binmeyecek.
+
+                sayi1 = Convert.ToInt32(txtSayi1.Text);     //Bu kısım tanımlı değişkenlere verileri aktarma kısmı.
+                sayi2 = Convert.ToInt32(txtSayi2.Text);
+                kat = Convert.ToInt32(cmbKat.SelectedItem); //Combobox içindeki veriler de stringten geliyor seçili veriyi int çevirip kat değişkenine aktarıyoruz.
+
+                if (sayi1 < sayi2) //Bu if bloklarının amacı kullanıcı sayıyı girerken hangi sayının büyük veya küçükse ona göre artarak veya azalarak sonuç göstermesini sağlamak. Eşit değerler ise programı da çalıştırmaz.
+                {
+                    for (; sayi1 <= sayi2; sayi1++) //döngü şartı küçük sayı <= büyük sayı olduğu sürece küçüğü 1 arttır. 
+                    {
+                        if (sayi1 % kat == 0) rtxtValue.Text += " " + sayi1 + " "; //Küçük sayı artarken combobox'tan gelen kat değeri ile tam bölündüğünde richtext'e sayi1'i ekle. " " : Boşluk kalması için.
+                    }
+                    MessageBox.Show(txtSayi1.Text + " ile " + txtSayi2.Text + " sayıları arasında olan " + cmbKat.SelectedItem + " ve katları hesaplandı.", "İşlem Tamamlandı!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } //richtexbox'a ilk textbox'daki sayının artarak giden katlarını göstericek. Çünkü küçük sayı.
+
+
+                else if (sayi1 > sayi2) //Bu durumda ilk girilen textbox 2. girilen textbox'tan büyük ise olan durum. Böylece en önemlisi programın çalışmama durumunu engellemiş olduk.
+                {
+
+                    for (; sayi2 <= sayi1; sayi1--) //döngü şartı küçük sayı <= büyük sayı olduğu sürece büyüğü 1 eksilt.
+                    {
+                        if (sayi1 % kat == 0) rtxtValue.Text += " " + sayi1 + " "; //büyük sayı azalırken combobox'tan gelen kat değeri ile tam bölündüğünde richtext'e sayi1'i ekle. " " : Boşluk kalması için.
+                    }
+                    MessageBox.Show(txtSayi1.Text + " ile " + txtSayi2.Text + " sayıları arasında olan " + cmbKat.SelectedItem + " ve katları hesaplandı.", "İşlem Tamamlandı!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }//richtexbox'a ilk textbox'daki sayının azalarak giden katlarını göstericek. Çünkü büyük sayı.
+
+
+                else MessageBox.Show("Sayılar Aynı Girilemez!", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Error);// Eğer iki sayıda birbirine eşitse bu uyarıyı verir.
+
+
+
+            }
+        }
+
+        private void rdBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdBlue.Checked) rtxtValue.ForeColor = Color.Blue; //Mavi
+        }
+
+        private void rdRed_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdRed.Checked) rtxtValue.ForeColor = Color.Red; //Kırmızı yazan buton seçilirse. RichTextbox içindeki verilerin rengini kırmızı yap.
+        }
+    }
+}
